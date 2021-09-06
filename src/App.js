3fdc7e13adc4ffc13d5e0172fdd23d164f91897a
Leandro Barbosa <x7leandro@hotmail.com.br>
2021-09-06 20:52:58 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -21,19 +21,23 @@ function App() {
     
     
     setRepositories([...repositories, response.data]);
-
-  async function handleRemoveRepository(id) {
-    
   }
-}
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ));
+  }
+
   return (
     <div>
       <ul data-testid="repository-list">
-       {repositories.map(repositorie => (
-          <li key={repositorie.id}>
-          {repositorie.title}
+       {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
